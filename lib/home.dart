8f03636b3df9b694d758f7f98fe0bdc359784336
Aditya Darma <adhit.boys1@gmail.com>
2020-06-01 01:16:53 +0800

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:sitepi/login.dart';
+import 'package:latlong/latlong.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,8 +17,30 @@ class _HomeState extends State<Home> {
       Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => Login()));
     });
-
   }
+
+  void _tripModalButtomSheet(context){
+    showModalBottomSheet(context: context, builder: (BuildContext bc){
+      return Container(
+        height: MediaQuery.of(context).size.height * .30,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('data', style: TextStyle(fontSize: 30, color: Colors.red),)
+        ),
+      );
+    });
+  }
+
+  void getDatabase(){
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +56,40 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Container(),
+      body: new FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(-8.7004,115.2207),
+          zoom: 13.50,
+        ),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://api.mapbox.com/styles/v1/adityadarma/ckav73s6rd90n1io1vagquve5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWRpdHlhZGFybWEiLCJhIjoiY2s3bjlheTB1MHFqMzNmbzB4dnNxcXNsYiJ9.hp7Hswgy29gHzNvEmK-YGg",
+            additionalOptions: {
+              'accessToken': 'pk.eyJ1IjoiYWRpdHlhZGFybWEiLCJhIjoiY2s3bjlheTB1MHFqMzNmbzB4dnNxcXNsYiJ9.hp7Hswgy29gHzNvEmK-YGg',
+              'id': 'mapbox.mapbox-streets-v7',
+            },
+          ),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 45.0,
+                height: 45.0,
+                point: new LatLng(-8.7004,115.2207),
+                builder: (ctx) =>
+                new Container(
+                  child: IconButton(
+                    icon: Icon(Icons.location_on),
+                    color: Colors.red,
+                    iconSize: 45.0,
+                    onPressed: (){
+                      _tripModalButtomSheet(context);
+                    }),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
